973ba0224a5bfcb133f9e408afc14b2912ebe0c4
Earl Ford <earl.f.student@lyonscommunityschool.org>
2017-03-21 14:12:48 +0000

    my comment

---
+++ script.js
@@ -0,0 +1,23 @@
+// JavaScript File
+//Create password variable
+//get value inside input (use jquery look at ref doc)
+//use if statement to check if value inside input equals the passwored
+
+$(document).ready(function () {
+$("#button").click(function() {
+
+var name = $("#input").val();
+
+ var password = "password";
+ 
+ if (password === name) {
+ alert("Congrats");
+ }
+//  else{
+//      alert('workjelkadjf')
+//  }
+});
+// JavaScript File
+
+  // All code goes here
+});
